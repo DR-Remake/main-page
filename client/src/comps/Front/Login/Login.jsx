84ca5a49a1054fd6f979045d272f164c28adef8f
Yosef <yosefplayz4580@gmail.com>
2023-07-15 21:13:17 +0300

    cool

--- client/src/comps/Front/Login/Login.jsx
+++ client/src/comps/Front/Login/Login.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import Register from "../Register/Register";
-import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const navigate = useNavigate();
   return (
-    <div>
-      <button onClick={() => navigate("/user/register")}>register</button>
-
-      <Register excludeInputs={['username', 'confirmPassword', 'date']} />
+    <div className="LoginComp combineW combineH">
+      <Register excludeInputs={["username", "confirmPassword", "date"]} />
     </div>
   );
 }
--- client/src/comps/Front/Register/Register.jsx
+++ client/src/comps/Front/Register/Register.jsx
@@ -59,23 +59,60 @@ export default function Register({ excludeInputs = [] }) {
   };
 
   return (
-    <div className="RegisterComp">
-      <div className="title">
-        <p>{excludeInputs.length !== 0 ? 'Login' : 'Register'}</p>
+    <div className="RegisterComp combineW combineH flex jcac">
+      <div className="square">
+        <div className="title flex">
+          <p className="gradientText">
+            {excludeInputs.length !== 0 ? "Login" : "Register"}
+          </p>
+          {excludeInputs.length === 0 ? (
+            <>
+              <h3>Already a rampager?</h3>
+              <button onClick={() => navigate("/user/login")}>Login</button>
+            </>
+          ) : (
+            <button onClick={() => navigate("/user/register")}>Register</button>
+          )}
+          <button onClick={() => navigate("/drr/home")}>home</button>
+        </div>
+        <form className="registerForm flex">
+          {theInputs.map((input, index) =>
+            excludeInputs.includes(input.name) ? null : (
+              <div className="tab combineW combineH flex">
+                <div className="left">
+                  {index === 0 && (
+                    <>
+                      <p>
+                        You wake up on a weird place, know nothing about what
+                        happened to you.
+                      </p>
+                      <p>
+                        You start looking around, and by time goes it gets more
+                        and more confusing.
+                      </p>
+                      <p>Suddenly, a blurry dark creature arrives near you.</p>
+                      <p>
+                        As they get closer, a windy voice reaches your mind ~{" "}
+                        <span>Who is this brave soul that violated my peace?</span>
+                      </p>
+                      <input
+                        key={input.name}
+                        placeholder={input.name}
+                        value={useSelector((state) => state.inputs[input.name])}
+                        type="text"
+                        onChange={(e) => handleInputChange(input.name, e)}
+                      />
+                    </>
+                  )}
+                </div>
+                <div className="right">
+                  <p>right</p>
+                </div>
+              </div>
+            )
+          )}
+        </form>
       </div>
-      {/* <button onClick={() => navigate(excludeInputs !== 0 ? "/drr/home" : '')}>home</button> */}
-      <form>
-        {theInputs.map((input) =>
-          excludeInputs.includes(input.name) ? null : (
-            <input
-              key={input.name}
-              value={useSelector((state) => state.inputs[input.name])}
-              type="text"
-              onChange={(e) => handleInputChange(input.name, e)}
-            />
-          )
-        )}
-      </form>
     </div>
   );
 }
